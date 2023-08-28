c32646b60cbc268148e95e84356e4e939bea2b47
ngocnhat2k1 <1951120116@sv.ut.edu.vn>
2023-08-29 01:37:39 +0700

    remove footer

--- admin/src/pages/Login.js
+++ admin/src/pages/Login.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../App.css';
-import Footer from '../Components/Footer';
 import CommonBanner from '../Components/CommonBanner';
 import LoginArea from '../Components/LoginArea';
 import Cookies from 'js-cookie';
@@ -17,7 +16,6 @@ const Login = () => {
     < div className="App" style={{ padding: 0 }} >
       < CommonBanner namePage="Login" />
       <LoginArea />
-      <Footer />
     </div>
   )
 }
